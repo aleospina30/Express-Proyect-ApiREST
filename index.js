b6813c1f05febc412624f02661f48b5a96ab5154
alejospina30 <alejospina08@gmail.com>
2023-12-20 18:01:47 -0500

    recibir parametros - parametros Query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const  { faker } = require('@faker-js/faker');
 const app = express();
 const port = 3000;
 
@@ -11,17 +12,58 @@ app.get('/nueva-ruta',(req,res)=> {
   res.send('Nueva ruta ')
 });
 
-
 app.get('/products',(req,res)=> {
-  res.json({
-    name: 'Producto 1',
-    price: 1000,
-    category: "Shoes"
-  })
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for(let index = 0; index < limit; index++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      Image: faker.image.imageUrl()
+    });
+  }
+  res.json(products);
+});
+
+app.get('/products/filter', (req,res) =>{
+  res.send('Soy un filter')
+})
+
+app.get('/products/:id', (req,res) => {
+  const { id } = req.params;
+  res.json(
+    {
+    id,
+    name: 'Producto 2',
+    price: 2000,
+    });
 });
 
 
+
+app.get('/users', (req,res) =>{
+  const { limit, offset } = req.query;
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send('No hay parametros')
+  }
+});
+
+app.get('/categories/:categoryId/products/:productId/orden-compra/:compraId', (req,res) =>{
+  const { categoryId, productId, compraId } = req.params;
+  res.json({
+  categoryId,
+  productId,
+  compraId
+    });
+  });
 
 app.listen(port,() =>{
   console.log('Mi port ' + port);
-});
+  });
+
